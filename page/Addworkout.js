6157d72caccd06d63c0d52d92757120872ec31b2
Ezarsatria12 <Ezar3004>
2024-06-30 22:36:53 +0700

    Addworkout

--- page/Addworkout.js
+++ page/Addworkout.js
@@ -8,41 +8,52 @@ import {
   ScrollView,
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import RNPickerSelect from "react-native-picker-select";
+
+import Modal from 'react-native-modal';
 
 const Addworkout = ({ navigation }) => {
   const [title, setTitle] = useState("");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [time, setTime] = useState(new Date());
+  const [showTimePicker, setShowTimePicker] = useState(false);
   const [note, setNote] = useState("");
-  const [exercises, setExercises] = useState([
-    { name: "", reps: "", sets: "" },
-  ]);
+  const [exercises, setExercises] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [newExercise, setNewExercise] = useState({
+    name: "",
+    reps: "",
+    sets: "",
+  });
+  const exerciseOptions = [
+    { label: "Push Ups", value: "Push Ups" },
+    { label: "Squats", value: "Squats" },
+    { label: "Lunges", value: "Lunges" },
+    // Tambahkan latihan lainnya di sini
+  ];
 
   const handleAddExercise = () => {
-    setExercises([...exercises, { name: "", reps: "", sets: "" }]);
-  };
-
-  const handleExerciseChange = (index, field, value) => {
-    const newExercises = [...exercises];
-    newExercises[index][field] = value;
-    setExercises(newExercises);
+    setExercises([...exercises, newExercise]);
+    setNewExercise({ name: "", reps: "", sets: "" });
+    setShowModal(false);
   };
 
   const handleSaveWorkout = () => {
-    // Save the workout (you might want to integrate with a backend or state management)
     console.log("Workout saved:", { title, date, time, note, exercises });
     navigation.goBack();
   };
 
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.head}>
-        <TouchableOpacity onPress={() => navigation.navigate("AddWorkout")}>
-          <Icon name="left" size={30} color="#000" />
+      <View style={styles.headerContainer}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon name="arrow-back" size={30} color="#000" />
         </TouchableOpacity>
         <Text style={styles.header}>Add Workout</Text>
-        <TouchableOpacity onPress={() => navigation.navigate("AddWorkout")}>
-          <Icon name="add" size={30} color="#000" />
+        <TouchableOpacity onPress={handleSaveWorkout}>
+          <Icon name="checkmark" size={30} color="#000" />
         </TouchableOpacity>
       </View>
       <TextInput
@@ -51,56 +62,141 @@ const Addworkout = ({ navigation }) => {
         value={title}
         onChangeText={setTitle}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Date"
-        value={date}
-        onChangeText={setDate}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Time"
-        value={time}
-        onChangeText={setTime}
-      />
+      <TouchableOpacity onPress={() => setShowDatePicker(true)}>
+        <TextInput
+          style={styles.input}
+          placeholder="Date"
+          value={date.toDateString()}
+          editable={false}
+          pointerEvents="none"
+        />
+      </TouchableOpacity>
+      {showDatePicker && (
+        <DateTimePicker
+          value={date}
+          mode="date"
+          display="default"
+          onChange={(event, selectedDate) => {
+            setShowDatePicker(false);
+            if (selectedDate) setDate(selectedDate);
+          }}
+        />
+      )}
+      <TouchableOpacity onPress={() => setShowTimePicker(true)}>
+        <TextInput
+          style={styles.input}
+          placeholder="Time"
+          value={time.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+          editable={false}
+          pointerEvents="none"
+        />
+      </TouchableOpacity>
+      {showTimePicker && (
+        <DateTimePicker
+          value={time}
+          mode="time"
+          display="default"
+          onChange={(event, selectedTime) => {
+            setShowTimePicker(false);
+            if (selectedTime) setTime(selectedTime);
+          }}
+        />
+      )}
       <TextInput
         style={styles.input}
         placeholder="Note"
         value={note}
         onChangeText={setNote}
       />
-      <Text style={styles.subHeader}>Exercises</Text>
+      <View style={styles.addExerciseHeader}>
+        <Text style={styles.subHeader}>Exercises</Text>
+        <TouchableOpacity onPress={() => setShowModal(true)}>
+          <Icon name="add-circle" size={30} color="#000" />
+        </TouchableOpacity>
+      </View>
       {exercises.map((exercise, index) => (
-        <View key={index} style={styles.exerciseContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Exercise Name"
-            value={exercise.name}
-            onChangeText={(text) => handleExerciseChange(index, "name", text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Reps"
-            value={exercise.reps}
-            onChangeText={(text) => handleExerciseChange(index, "reps", text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Sets"
-            value={exercise.sets}
-            onChangeText={(text) => handleExerciseChange(index, "sets", text)}
-          />
+        <View key={index} style={styles.exerciseCard}>
+          <Text style={styles.exerciseText}>{exercise.name}</Text>
+          <Text style={styles.exerciseText}>Reps: {exercise.reps}</Text>
+          <Text style={styles.exerciseText}>Sets: {exercise.sets}</Text>
         </View>
       ))}
-      <TouchableOpacity style={styles.addButton} onPress={handleAddExercise}>
-        <Text style={styles.addButtonText}>Add Exercise</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.saveButton} onPress={handleSaveWorkout}>
-        <Text style={styles.saveButtonText}>Save Workout</Text>
-      </TouchableOpacity>
+      <Modal
+        isVisible={showModal}
+        onBackdropPress={() => setShowModal(false)}
+        animationIn="slideInUp"
+        animationOut="slideOutDown"
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalTitle}>Add Exercise</Text>
+              <TouchableOpacity onPress={() => setShowModal(false)}>
+                <Icon name="close" size={30} color="#000" />
+              </TouchableOpacity>
+            </View>
+            <RNPickerSelect
+              onValueChange={(value) =>
+                setNewExercise({ ...newExercise, name: value })
+              }
+              items={exerciseOptions}
+              style={pickerSelectStyles}
+              placeholder={{ label: "Select Exercise", value: null }}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Reps"
+              value={newExercise.reps}
+              onChangeText={(text) =>
+                setNewExercise({ ...newExercise, reps: text })
+              }
+              keyboardType="numeric"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Sets"
+              value={newExercise.sets}
+              onChangeText={(text) =>
+                setNewExercise({ ...newExercise, sets: text })
+              }
+              keyboardType="numeric"
+            />
+            <TouchableOpacity
+              style={styles.saveButton}
+              onPress={handleAddExercise}
+            >
+              <Text style={styles.saveButtonText}>Save Exercise</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </ScrollView>
   );
 };
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    fontSize: 16,
+  },
+  inputAndroid: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    fontSize: 16,
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -109,15 +205,15 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     backgroundColor: "#fff",
   },
-  head: {
+  headerContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
   },
   header: {
     fontSize: 24,
     fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 20,
   },
   input: {
     height: 40,
@@ -127,30 +223,63 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     marginBottom: 15,
   },
+  addExerciseHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 20,
+  },
   subHeader: {
     fontSize: 18,
     fontWeight: "bold",
-    marginBottom: 10,
   },
-  exerciseContainer: {
+  exerciseCard: {
+    backgroundColor: "#f9f9f9",
+    padding: 15,
+    marginVertical: 10,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  exerciseText: {
+    fontSize: 16,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  modalHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 20,
   },
-  addButton: {
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  saveButton: {
     backgroundColor: "#4CAF50",
     padding: 15,
     borderRadius: 10,
     alignItems: "center",
-    marginBottom: 20,
-  },
-  addButtonText: {
-    color: "#fff",
-    fontWeight: "bold",
-  },
-  saveButton: {
-    backgroundColor: "#2196F3",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
+    marginTop: 20,
   },
   saveButtonText: {
     color: "#fff",
